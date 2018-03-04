5e985a6195301dbb0052d6d57b85c8a68375328a
Jobrann Mous <jobrann.mous@hotmail.com>
2018-03-04 22:08:51 +0100

    added views and controlelr varibales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
   def top
     @articles = Article.where(publication_date: Date.today).order(total_views: :desc).first(10)
     @top_cointelegraph = Article.where(publication_date:Date.today).where(source:"Coin Telegraph").order(total_views: :desc).first(5)
+    @top_coindesk = Article.where(publication_date:Date.today).where(source:"CoinDesk").order(tw_count: :desc).first(5)
     @top_bitcoin = Article.where(publication_date:Date.today).where(source:"Bitcoin.com").order(total_views: :desc).first(5)
   end
 
