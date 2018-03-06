08df058c45a5a84ecd4484bbc07d20dbcd5427d7
Jobrann Mous <jobrann.mous@hotmail.com>
2018-03-06 15:21:00 +0100

    setup capybara for heroku

--- app/services/articles_scraper_service.rb
+++ app/services/articles_scraper_service.rb
@@ -144,8 +144,15 @@ class ArticlesScraperService
       end
     end
 
+    chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+    options = {}
+    options[:args] = ['headless', 'disable-gpu', 'window-size=1280,1024']
+    options[:binary] = chrome_bin if chrome_bin
     Capybara.register_driver :selenium do |app|
-      Capybara::Selenium::Driver.new(app, browser: :chrome)
+      Capybara::Selenium::Driver.new(app,
+       browser: :chrome,
+       options: Selenium::WebDriver::Chrome::Options.new(options)
+       )
     end
     Capybara.default_driver = :selenium
     browser = Capybara.current_session
